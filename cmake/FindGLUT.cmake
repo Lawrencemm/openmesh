78196aaaa28375ddaf3e7a3a8570941c15353cfb
Jan Möbius <moebius@cs.rwth-aachen.de>
2016-04-28 11:03:48 +0200

    Typo

--- cmake/FindGLUT.cmake
+++ cmake/FindGLUT.cmake
@@ -46,7 +46,7 @@ IF (WIN32)
   FIND_PATH( GLUT_INCLUDE_DIR NAMES GL/glut.h 
     PATHS  ${GLUT_ROOT_PATH}/include 
            "${CMAKE_WINDOWS_LIBS_DIR}/glut-3.7/include"
-           "${VS_SEARCH_PATH}/freeglut-3.0.0/include" )
+           "${VS_SEARCH_PATH}/freeglut-3.0.0/include" 
            "${VS_SEARCH_PATH}/freeglut-2.8.1/include" )
 
   FIND_LIBRARY( GLUT_glut_LIBRARY NAMES glut32 glut freeglut
