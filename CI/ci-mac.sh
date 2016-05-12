33cdd611ab4ec9fb4fdecaed826bda48b876b0eb
Jan Möbius <moebius@cs.rwth-aachen.de>
2016-05-12 11:01:33 +0200

    More verbose ci output for configuration

--- CI/ci-mac.sh
+++ CI/ci-mac.sh
@@ -13,11 +13,11 @@ OPTIONS=""
 
 if [ "$LANGUAGE" == "C++98" ]; then
   echo "Building with C++98";
-  BUILDPATH="$BUILDPATH-cpp98"
+  BUILDPATH="cpp98"
 elif [ "$LANGUAGE" == "C++11" ]; then
   echo "Building with C++11";
   OPTIONS="$OPTIONS -DCMAKE_CXX_FLAGS='-std=c++11' "
-  BUILDPATH="$BUILDPATH-cpp11"  
+  BUILDPATH="cpp11"  
 fi  
 
 #=====================================
@@ -27,6 +27,18 @@ NC='\033[0m'
 OUTPUT='\033[0;32m'
 WARNING='\033[0;93m'
 
+
+echo -e "${OUTPUT}"
+echo ""
+echo "======================================================================"
+echo "Basic configuration details:"
+echo "======================================================================"
+echo -e "${NC}"
+
+echo "Options:    $OPTIONS"
+echo "BuildPath:  $BUILDPATH"
+echo "Path:       $PATH"
+echo "Language:   $LANGUAGE"
 
 echo -e "${OUTPUT}"
 echo ""
@@ -108,7 +120,7 @@ else
   echo -e "${NC}"
 fi
 
-
+OPTIONS=
 cd ..
 
 
