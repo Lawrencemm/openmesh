122293b2c0ddfbb6539513fafa5ba483a815fd12
Martin Schultz <Martin.Schultz@rwth-aachen.de>
2016-12-08 18:17:11 +0100

    added copy constructor to PolyMesh_ArrayKernelT and TriMesh_ArrayKernelT

--- src/OpenMesh/Core/Mesh/TriMesh_ArrayKernelT.hh
+++ src/OpenMesh/Core/Mesh/TriMesh_ArrayKernelT.hh
@@ -74,7 +74,8 @@
 
 namespace OpenMesh {
 
-
+template<class Traits>
+class PolyMesh_ArrayKernelT;
 //== CLASS DEFINITION =========================================================
 
 
@@ -97,7 +98,18 @@ struct TriMesh_ArrayKernel_GeneratorT
 template <class Traits = DefaultTraits>
 class TriMesh_ArrayKernelT
   : public TriMesh_ArrayKernel_GeneratorT<Traits>::Mesh
-{};
+{
+public:
+  TriMesh_ArrayKernelT() {}
+  template<class OtherTraits>
+   TriMesh_ArrayKernelT( const PolyMesh_ArrayKernelT<OtherTraits> & t)
+  {
+     //assign the connectivity (add vertices)
+     this->assign(t);
+     //copy properties from polyMesh
+     this->copy_all_kernel_properties(t);
+  }
+};
 
 
 //=============================================================================
