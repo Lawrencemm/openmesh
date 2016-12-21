9de0f264d87e004d075ed16cb6b832d43d5dc90b
Jan Möbius <moebius@cs.rwth-aachen.de>
2016-12-21 10:04:27 +0100

    Doc Build

---
+++ CI/ci-doc.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+if [ ! -d build-release ]; then
+  mkdir build-release
+fi
+
+cd build-release
+
+cmake -DCMAKE_BUILD_TYPE=Release -DOPENMESH_BUILD_UNIT_TESTS=FALSE ../
+
+make doc
+
+# Copy to webserver
+scp -r -p2222 Build/share/OpenMesh/Doc/html/* hudson@web4-info8:/OpenMesh-Daily/Doc/
