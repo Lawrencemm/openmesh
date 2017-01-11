3b906e51199363e33299a3f9bf3b96b0a3dc8de1
Janis Born <born@cs.rwth-aachen.de>
2017-01-11 17:26:28 +0100

    use double as default RealType for subdividers

--- src/OpenMesh/Tools/Subdivider/Uniform/ModifiedButterFlyT.hh
+++ src/OpenMesh/Tools/Subdivider/Uniform/ModifiedButterFlyT.hh
@@ -95,7 +95,7 @@ namespace Uniform    { // BEGIN_NS_UNIFORM
  *
  * Clement Courbet - clement.courbet@ecp.fr
  */
-template <typename MeshType, typename RealType = float>
+template <typename MeshType, typename RealType = double>
 class ModifiedButterflyT : public SubdividerT<MeshType, RealType>
 {
 public:
