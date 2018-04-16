74350f54de749c14d466bbb6e75c77fe0342107b
Christian Mattes <christian.mattes@rwth-aachen.de>
2018-04-16 13:21:54 +0200

    Proper spacing between methods

--- src/OpenMesh/Core/Geometry/VectorT.hh
+++ src/OpenMesh/Core/Geometry/VectorT.hh
@@ -264,6 +264,7 @@ dot(const VectorT<Scalar,N>& _v1, const VectorT<Scalar,N>& _v2) {
   return (_v1 | _v2); 
 }
 
+
 /// \relates OpenMesh::VectorT
 /// symmetric version of the cross product
 template<typename Scalar, int N>
@@ -272,6 +273,7 @@ cross(const VectorT<Scalar,N>& _v1, const VectorT<Scalar,N>& _v2) {
   return (_v1 % _v2);
 }
 
+
 /// \relates OpenMesh::VectorT
 /// non-member norm
 template<typename Scalar, int DIM>
@@ -279,18 +281,22 @@ Scalar norm(const VectorT<Scalar, DIM>& _v) {
     return _v.norm();
 }
 
+
 /// \relates OpenMesh::VectorT
 /// non-member sqrnorm
 template<typename Scalar, int DIM>
 Scalar sqrnorm(const VectorT<Scalar, DIM>& _v) {
     return _v.sqrnorm();
 }
+
+
 /// \relates OpenMesh::VectorT
 /// non-member vectorize
 template<typename Scalar, int DIM, typename OtherScalar>
 VectorT<Scalar, DIM>& vectorize(VectorT<Scalar, DIM>& _v, OtherScalar const& _val) {
     return _v.vectorize(_val);
 }
+
 
 /// \relates OpenMesh::VectorT
 /// non-member normalize
@@ -299,6 +305,7 @@ VectorT<Scalar, DIM>& normalize(VectorT<Scalar, DIM>& _v) {
     return _v.normalize();
 }
 
+
 /// \relates OpenMesh::VectorT
 /// non-member maximize
 template<typename Scalar, int DIM>
@@ -306,12 +313,14 @@ VectorT<Scalar, DIM>& maximize(VectorT<Scalar, DIM>& _v1, VectorT<Scalar, DIM>& 
     return _v1.maximize(_v2);
 }
 
+
 /// \relates OpenMesh::VectorT
 /// non-member minimize
 template<typename Scalar, int DIM>
 VectorT<Scalar, DIM>& minimize(VectorT<Scalar, DIM>& _v1, VectorT<Scalar, DIM>& _v2) {
     return _v1.minimize(_v2);
 }
+
 
 //== TYPEDEFS =================================================================
 
